05b7bcec78129ab538b143fb093a44e856849835
bjoepen <bernd.joepen@t-online.de>
2026-09-16 20:59:06 +0200

    Build 005-C5d3b: expose playback completion bridge

--- ui/src/lib/projectBridge.js
+++ ui/src/lib/projectBridge.js
@@ -1,37 +1,15 @@
 import { invoke } from "@tauri-apps/api/core";
 
 export const projectBridge = {
-  loadProjectState() {
-    return invoke("load_project_state");
-  },
-
-  openProjectFile() {
-    return invoke("open_project_file");
-  },
-
-  saveProjectFile() {
-    return invoke("save_project_file");
-  },
-
-  syncManuscript(manuscript) {
-    return invoke("sync_manuscript", { manuscript });
-  },
-
-  setUnitVoice(unitId, voiceId) {
-    return invoke("set_unit_voice", { unitId, voiceId });
-  },
-
-  renderProjectUnit(unitId, previousAudio = null) {
-    return invoke("render_project_unit", { unitId, previousAudio });
-  },
-
-  playUnitAudio(unitId, audioPath) {
-    return invoke("play_unit_audio", { unitId, audioPath });
-  },
-
-  stopUnitAudio(unitId) {
-    return invoke("stop_unit_audio", { unitId });
-  },
+  loadProjectState() { return invoke("load_project_state"); },
+  openProjectFile() { return invoke("open_project_file"); },
+  saveProjectFile() { return invoke("save_project_file"); },
+  syncManuscript(manuscript) { return invoke("sync_manuscript", { manuscript }); },
+  setUnitVoice(unitId, voiceId) { return invoke("set_unit_voice", { unitId, voiceId }); },
+  renderProjectUnit(unitId, previousAudio = null) { return invoke("render_project_unit", { unitId, previousAudio }); },
+  playUnitAudio(unitId, audioPath) { return invoke("play_unit_audio", { unitId, audioPath }); },
+  stopUnitAudio(unitId) { return invoke("stop_unit_audio", { unitId }); },
+  isUnitAudioPlaying(unitId) { return invoke("is_unit_audio_playing", { unitId }); },
 };
 
 export const loadProjectState = () => projectBridge.loadProjectState();
@@ -39,8 +17,7 @@ export const openProjectFile = () => projectBridge.openProjectFile();
 export const saveProjectFile = () => projectBridge.saveProjectFile();
 export const syncManuscript = (manuscript) => projectBridge.syncManuscript(manuscript);
 export const setUnitVoice = (unitId, voiceId) => projectBridge.setUnitVoice(unitId, voiceId);
-export const renderProjectUnit = (unitId, previousAudio = null) =>
-  projectBridge.renderProjectUnit(unitId, previousAudio);
-export const playUnitAudio = (unitId, audioPath) =>
-  projectBridge.playUnitAudio(unitId, audioPath);
+export const renderProjectUnit = (unitId, previousAudio = null) => projectBridge.renderProjectUnit(unitId, previousAudio);
+export const playUnitAudio = (unitId, audioPath) => projectBridge.playUnitAudio(unitId, audioPath);
 export const stopUnitAudio = (unitId) => projectBridge.stopUnitAudio(unitId);
+export const isUnitAudioPlaying = (unitId) => projectBridge.isUnitAudioPlaying(unitId);
